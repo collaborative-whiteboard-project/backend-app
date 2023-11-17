cb8d502445332e0d7030252e7cde73b907a64cba
npolak <nikoem.polak@itsilesia.com>
2023-11-17 18:31:57 +0100

    Initialize Spring boot project

---
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/CollaborativeWebWhiteboardApplication.java
@@ -0,0 +1,13 @@
+package pl.polak.nikodem.whiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeWebWhiteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeWebWhiteboardApplication.class, args);
+	}
+
+}
---
+++ whiteboard/src/test/java/pl/polak/nikodem/whiteboard/CollaborativeWebWhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polak.nikodem.whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeWebWhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
