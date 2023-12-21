60c4814be81fecf05ea848fef7d4f773e73100cc
nikodem-pol <nw.polak@gmail.com>
2023-12-21 19:58:51 +0100

    Create DefaultExceptionHandler

---
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/exceptions/DefaultExceptionHandler.java
@@ -0,0 +1,33 @@
+package pl.polak.nikodem.whiteboard.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class DefaultExceptionHandler {
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleUserNotFoundException(UserNotFoundException exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<String> handleValidationException(MethodArgumentNotValidException exception) {
+        return exception
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+    }
+}
