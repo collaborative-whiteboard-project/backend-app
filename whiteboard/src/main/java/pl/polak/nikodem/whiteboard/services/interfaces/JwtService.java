1ddfa7716a39a302cd4ef599127478d670990094
npolak <nikoem.polak@itsilesia.com>
2023-12-21 14:17:17 +0100

    Remove unused imports

--- whiteboard/src/main/java/pl/polak/nikodem/whiteboard/services/interfaces/JwtService.java
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/services/interfaces/JwtService.java
@@ -1,17 +1,6 @@
 package pl.polak.nikodem.whiteboard.services.interfaces;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
 
 public interface JwtService {
     String extractUsername(String token);
