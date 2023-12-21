2a7ecdfb2b04a1cc0ccf6c6d2c250e1249480b4a
nikodem-pol <nw.polak@gmail.com>
2023-12-21 19:53:14 +0100

    Revert "Create UserNotFoundException"
    This reverts commit 7bca2741ab3d4fbf0e784c597281f866d43eeedc.

--- whiteboard/src/main/java/pl/polak/nikodem/whiteboard/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package pl.polak.nikodem.whiteboard.exceptions;
-
-public class UserNotFoundException extends Exception{
-
-    public UserNotFoundException(String msg) {
-        super(msg);
-    }
-}
