7bca2741ab3d4fbf0e784c597281f866d43eeedc
nikodem-pol <nw.polak@gmail.com>
2023-12-21 19:52:16 +0100

    Create UserNotFoundException

---
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package pl.polak.nikodem.whiteboard.exceptions;
+
+public class UserNotFoundException extends Exception{
+
+    public UserNotFoundException(String msg) {
+        super(msg);
+    }
+}
