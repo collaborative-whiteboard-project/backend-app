8b3c10c7674c8095582732bebcfd3c6d8bfd3bc0
nikodem-pol <nw.polak@gmail.com>
2023-12-21 19:58:02 +0100

    Add validation for dtos fields

--- whiteboard/src/main/java/pl/polak/nikodem/whiteboard/dtos/SignInRequest.java
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/dtos/SignInRequest.java
@@ -1,5 +1,9 @@
 package pl.polak.nikodem.whiteboard.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,6 +12,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignInRequest {
-    String email;
-    String password;
+    @Email(message = "not_an_email")
+    @NotEmpty(message = "email_is_empty")
+    @NotNull(message = "email_is_null")
+    private String email;
+
+    @Size(min = 8, message = "password_too_short")
+    @Size(max = 100, message = "password_too_long")
+    @NotNull(message = "password_is_null")
+    private String password;
 }
