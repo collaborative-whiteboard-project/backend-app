3f78abf14c04281d216722847e60271f61bc8159
nikodem-pol <nw.polak@gmail.com>
2023-12-30 23:08:22 +0100

    Create ProjectController class

---
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/controllers/ProjectController.java
@@ -0,0 +1,61 @@
+package pl.polak.nikodem.whiteboard.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.polak.nikodem.whiteboard.dtos.project.*;
+import pl.polak.nikodem.whiteboard.exceptions.*;
+import pl.polak.nikodem.whiteboard.services.interfaces.ProjectService;
+import pl.polak.nikodem.whiteboard.services.interfaces.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/project")
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+    private final UserService userService;
+
+    @GetMapping("/user/all")
+    public List<SimpleProjectResponse> getUserProjects() throws UserNotAuthenticatedException, UserNotFoundException {
+        String email = userService.getAuthenticatedUserEmail();
+        return projectService.getUserProjects(email);
+    }
+
+    @GetMapping("/content/{id}")
+    public ProjectContentResponse getProjectContent(@PathVariable Long id) throws ProjectNotFoundException, UserNotAuthenticatedException, UserNotAProjectMemberException, InsufficientProjectMemberRoleException {
+        String email = userService.getAuthenticatedUserEmail();
+        return projectService.getProjectContent(id, email);
+    }
+
+    @GetMapping("/{id}/members")
+    public List<ProjectMemberResponse> getProjectMembers(@PathVariable Long id) throws UserNotAuthenticatedException, ProjectNotFoundException, UserNotAProjectMemberException, InsufficientProjectMemberRoleException {
+        String email = userService.getAuthenticatedUserEmail();
+        return projectService.getProjectMembers(id, email);
+    }
+
+    @GetMapping("/{id}/owners")
+    public List<ProjectMemberResponse> getProjectOwners(@PathVariable Long id) throws UserNotAuthenticatedException, ProjectNotFoundException, UserNotAProjectMemberException, InsufficientProjectMemberRoleException {
+        String email = userService.getAuthenticatedUserEmail();
+        return projectService.getProjectOwners(id, email);
+    }
+
+    @PostMapping("/create")
+    public void createProject(@RequestBody @Valid CreateProjectRequest createProjectRequest) throws UserNotAuthenticatedException, UserNotFoundException {
+        String email = userService.getAuthenticatedUserEmail();
+        projectService.createNewProject(email, createProjectRequest.getProjectName());
+    }
+
+    @PostMapping("/{id}/members/add")
+    public void addProjectMembers(@PathVariable Long id, @RequestBody @Valid AddProjectMembersRequest request) throws UserNotAuthenticatedException, UserNotFoundException, ProjectNotFoundException, UserNotAProjectMemberException, InsufficientProjectMemberRoleException {
+        String email = userService.getAuthenticatedUserEmail();
+        projectService.addProjectMembers(id, email, request.getMembers());
+    }
+
+    @DeleteMapping("/{id}/members/delete")
+    public void deleteProjectMembers(@PathVariable Long id, @RequestBody @Valid DeleteProjectMembersRequest request) throws UserNotAuthenticatedException, ProjectNotFoundException, UserNotAProjectMemberException, InsufficientProjectMemberRoleException {
+        String email = userService.getAuthenticatedUserEmail();
+        projectService.deleteProjectMembers(id, email, request.getEmails());
+    }
+}
