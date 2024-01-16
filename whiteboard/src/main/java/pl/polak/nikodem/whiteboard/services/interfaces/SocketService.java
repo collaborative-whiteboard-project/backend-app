7b610e0764d638df4340e0fe4ed5560db4443ea5
nikodem-pol <nw.polak@gmail.com>
2024-01-17 00:44:40 +0100

    Create SocketService interface

---
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/services/interfaces/SocketService.java
@@ -0,0 +1,16 @@
+package pl.polak.nikodem.whiteboard.services.interfaces;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import pl.polak.nikodem.whiteboard.dtos.socket.WhiteboardOperationData;
+import pl.polak.nikodem.whiteboard.exceptions.InsufficientProjectMemberRoleException;
+import pl.polak.nikodem.whiteboard.exceptions.ProjectNotFoundException;
+import pl.polak.nikodem.whiteboard.exceptions.UserNotAProjectMemberException;
+import pl.polak.nikodem.whiteboard.exceptions.UserNotAuthenticatedException;
+
+public interface SocketService {
+
+    void joinRoom(SocketIOClient client, String jwtToken, String projectId) throws UserNotAuthenticatedException, ProjectNotFoundException, UserNotAProjectMemberException, InsufficientProjectMemberRoleException;
+    void leaveRoom(SocketIOClient client, String jwtToken, String projectId) throws UserNotAuthenticatedException, ProjectNotFoundException;
+    void disconnectUser(SocketIOClient client) throws ProjectNotFoundException;
+    void editWhiteboard(SocketIOClient client, WhiteboardOperationData data);
+}
