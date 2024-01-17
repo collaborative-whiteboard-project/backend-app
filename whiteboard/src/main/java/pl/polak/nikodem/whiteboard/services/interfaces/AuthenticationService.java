90f9592294c8950414192f0785eda925a6564db1
nikodem-pol <nw.polak@gmail.com>
2024-01-17 21:59:07 +0100

    Add sendResetPasswordEmail and resetUserPassword methods

--- whiteboard/src/main/java/pl/polak/nikodem/whiteboard/services/interfaces/AuthenticationService.java
+++ whiteboard/src/main/java/pl/polak/nikodem/whiteboard/services/interfaces/AuthenticationService.java
@@ -1,17 +1,14 @@
 package pl.polak.nikodem.whiteboard.services.interfaces;
 
-import pl.polak.nikodem.whiteboard.dtos.auth.ChangePasswordRequest;
-import pl.polak.nikodem.whiteboard.dtos.auth.JwtAuthenticationResponse;
-import pl.polak.nikodem.whiteboard.dtos.auth.SignInRequest;
-import pl.polak.nikodem.whiteboard.dtos.auth.SignUpRequest;
+import pl.polak.nikodem.whiteboard.dtos.auth.*;
 import pl.polak.nikodem.whiteboard.exceptions.UserNotAuthenticatedException;
 import pl.polak.nikodem.whiteboard.exceptions.UserNotFoundException;
 
 public interface AuthenticationService {
 
     JwtAuthenticationResponse signup(SignUpRequest request);
-
     JwtAuthenticationResponse signin(SignInRequest request);
-
     JwtAuthenticationResponse changePassword(ChangePasswordRequest request) throws UserNotAuthenticatedException, UserNotFoundException;
+    void sendResetPasswordEmail(SendResetPasswordEmailRequest request) throws UserNotFoundException;
+    void resetUserPassword(ResetPasswordRequest request) throws UserNotFoundException;
 }
